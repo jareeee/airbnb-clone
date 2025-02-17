75830c1396af14d9d273256a156de87599993ed1
jareeee <fajarmulyos@gmail.com>
2025-02-18 00:18:17 +0700

    Minor fixes to wishlist

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateWishlistStatus() {
+
+    const userLoggedIn = this.element.dataset.userLoggedIn;
+    if (userLoggedIn === "false") {
+      document.querySelector('.js-login').click();
+      return
+    }
     if (this.element.dataset.status === "false") {
       this.element.classList.remove("fill-none")
       this.element.classList.add("fill-[#FF5A5F]")
